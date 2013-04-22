9d281e9ef652bb1dbc2560ca3963cf0ad3426075
David Silver <dsilver@candidatemetrics.com>
2013-04-22 08:18:42 -0700

    dynamic error pages

--- store/app/controllers/application_controller.rb
+++ store/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from "ProductsController::Forbidden", with: :forbidden
+
+private
+
+  def forbidden(exception)
+    render text: exception.message
+  end
 end
